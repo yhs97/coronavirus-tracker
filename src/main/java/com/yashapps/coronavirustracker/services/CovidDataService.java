30aee3fa43a7a0d0b80982328caea3db798cf026
yhs97 <sarvaiyayash@gmail.com>
2020-09-28 12:56:31 -0700

    Added CovidDataService

--- src/main/java/com/yashapps/coronavirustracker/services/CovidDataService.java
+++ src/main/java/com/yashapps/coronavirustracker/services/CovidDataService.java
@@ -1,4 +1,26 @@
 package com.yashapps.coronavirustracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
 public class CovidDataService {
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+
+    @PostConstruct //When you construct an instance of this service, execute fetchCovidData()
+    public void fetchCovidData() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
+    }
 }
